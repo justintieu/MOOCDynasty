6df2f6bf75e6980984e569ca6905de22e0a14b30
justintieu <tieujustin@gmail.com>
2014-04-01 10:35:50 -0700

    Updated futurelearn

--- futurelearn.java
+++ futurelearn.java
@@ -34,83 +34,63 @@ public class futurelearn {
 			String furl = (String) pgcrs.get(a);
 			Document doc = Jsoup.connect(furl).get();
 			Elements ele = doc.select("li[class^=media]");
-			Elements crspg = ele.select("h2[class=title]");
-			Elements link = crspg.select("a[href]");
-			
+			Elements crspg = ele.select("h2[class^=title]");
+			Elements link = crspg.select("a");
 			for (int j=0; j<link.size();j++)
 			{
-				//Statement statement = connection.createStatement();
+				Statement statement = connection.createStatement();
 			    
 				String crsurl = "https://www.futurelearn.com" +link.get(j).attr("href"); //Get the Course Url from href tag
-				System.out.println(crsurl);
-				String CourseName = crspg.select("h1").get(j).text(); //Get the Course Name from H1 Tag
+				//System.out.println(crsurl);
+				String CourseName = crspg.get(j).text(); //Get the Course Name 
 				CourseName = CourseName.replace("'", "''");
-				String SCrsDesrpTemp = crspg.select("div[class=subtitle]").get(j).text();
-				CourseName = CourseName.replace(",", "");
-				SCrsDesrpTemp = SCrsDesrpTemp.replace("?", "");
-				//String SCrsDesrp = SCrsDesrpTemp.substring(0, (SCrsDesrpTemp.length()-5)); //To get the course description and remove the extra characters at the end.
+				String SCrsDesrpTemp = doc.select("p[class=introduction").get(j).text();	//Get short course description
 				SCrsDesrpTemp = SCrsDesrpTemp.replace("'", "''");
 				SCrsDesrpTemp = SCrsDesrpTemp.replace(",", "");
-				String CrsImg  = crspg.select("a[class=media_image] > img").get(j).attr("src");; //To get the course image 
+				String CrsImg  = doc.select("a[class=media_image] > img").get(j).attr("src"); //To get the course image 
+				
 				Document crsdoc = Jsoup.connect(crsurl).get();
 				//Elements crsheadele = crsdoc.select("section[class=course-header clearfix]");
 				String youtube = crsdoc.select("div[class=video-step-container] > iframe").attr("src"); //Youtube link
 				//Elements crsbodyele = crsdoc.select("section[class=course-detail clearfix]");
-				String CrsDes = crsdoc.select("div[class=course-description] > section[class=small]"); //Course Description Element
-				CrsDes = CrsDes.replace("'", "''");
-				CrsDes = CrsDes.replace(",", "");
+				String CrsDes = crsdoc.select("div[class=course-description] > section[class=small]").text(); //Course Description Element
 				if(CrsDes.contains("?"))
 				{
 					CrsDes = CrsDes.replace("?", "");
 				}
-				String Date = crsdoc.select("div[class=startdate]").text();
-				String StrDate = Date.substring(Date.indexOf(":")+1, Date.length()); //Start date after the :
-				String datechk = StrDate.substring(0, StrDate.indexOf(" "));
-				if(!datechk.matches(".*\\d.*"))
+				
+				String courseId = crsdoc.select("article").attr("id").substring(7);
+				String profImg = crsdoc.select("div[class=educator] > a > img").attr("src");
+				String profName = crsdoc.select("div[class=educator] > a > img").attr("alt");
+				
+				Elements findDuration = crsdoc.select("ul[class=list] > li > p");
+				
+				String StrDate = "";
+				String datechk = findDuration.get(0).text();
+				if(datechk.equals("Starts late 2014"))
 				{
-					if(StrDate.contains("n/a"))
-					{
-						StrDate = "write you own code";
-					}
-					else
-					{
-						StrDate = "write your own code";
-					}
+					StrDate = "2014-09-01";
 				}
 				else
 				{
-					String date = StrDate.substring(0, StrDate.indexOf(" "));
-					String month = StrDate.substring(StrDate.indexOf(" ")+1, StrDate.indexOf(" ")+4);
-					String year = StrDate.substring(StrDate.length()-4,StrDate.length());
-					StrDate = "write your own code";
+					StrDate = findDuration.get(0).select("time").attr("datetime");
 				}
-				Element chk = crsdoc.select("div[class=effort last]").first();
-				Element crslenschk = crsdoc.select("div[class*=duration]").first();
-				String crsduration;
-				if (crslenschk==null)
+				if(youtube.length() > 0)
 				{
-					crsduration = "0";
+					youtube = "http:" + youtube;
+				} 
+				else {
+					youtube = "n/a";
 				}
-				else if(StrDate.contains("n/a self-paced"))
-				{
-					crsduration = "0";
-				}
-				else
-				{
-					try{
-						String crsdurationtmp = crsdoc.select("div[class*=duration]").text();
-						int start = crsdurationtmp.indexOf(":")+1;
-						int end = crsdurationtmp.indexOf((" "),crsdurationtmp.indexOf(":"));
-						crsduration = crsdurationtmp.substring(start, end);
-					}
-					catch (Exception e)
-					{
-						crsduration ="0";
-						System.out.println("Exception");
-					}
-				}
-				String query = "insert into course_data values(null,'"+CourseName+"','"+SCrsDesrpTemp+"','"+CrsDes+"','"+crsurl+"','"+youtube+"',"+StrDate+","+crsduration+",'"+CrsImg+"','','FutureLearn')";
-				System.out.println(query);                	
+				String crsduration = findDuration.get(1).text().substring(findDuration.get(1).text().indexOf(':')+2, findDuration.get(1).text().indexOf(':')+3); //Get class duration and remove "Duration : "
+				String dataQuery = "INSERT INTO `scrapedcourse`.`course_data` (`id`, `title`, `short_desc`, `long_desc`, `course_link`, `video_link`, `start_date`, `course_length`, `course_image`, `category`, `site`) VALUES(NULL,'"+CourseName+"','"+SCrsDesrpTemp+"','"+CrsDes+"','"+crsurl+"','"+youtube+"','"+StrDate+"','"+crsduration+"','"+CrsImg+"','','FutureLearn')";
+				String detailsQuery = "INSERT INTO `scrapedcourse`.`coursedetails` (`id`,`profname`, `profimage`, `course_id`) VALUES(3,'" + profName + "', '" + profImg + "', '" + courseId + "')";
+//				System.out.println(dataQuery);       
+//				System.out.println(detailsQuery);       
+//				statement.executeUpdate(dataQuery); 
+//				statement.executeUpdate(detailsQuery);
+				String query = "INSERT INTO `scrapedcourse`.`data` (`id`, `title`, `short_desc`, `long_desc`, `course_link`, `video_link`, `start_date`, `course_length`, `course_image`, `category`, `site`, `profname`, `profimage`, `course_id`) VALUES (NULL,'"+CourseName+"','"+SCrsDesrpTemp+"','"+CrsDes+"','"+crsurl+"','"+youtube+"','"+StrDate+"','"+crsduration+"','"+CrsImg+"','','FutureLearn','" + profName + "', '" + profImg + "', '" + courseId + "')";
+				System.out.println(query);
 				statement.executeUpdate(query);
 				statement.close(); 
 			 }
