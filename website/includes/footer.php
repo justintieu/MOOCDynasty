b738de336f22a44d3289fff8379c74c3f39fa775
Justin Tieu <tieujustin@gmail.com>
2014-05-11 16:12:58 -0700

    Added SJSU and SJSU CS logo to footer

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,5 +1,9 @@
 <div id="footer">
 	<div class="inside">
-		<p>MoocDynasty &copy; 2014   | SJSU CS160</p>
+		<div style="display: table; float: right;">
+			<span style="display: table-cell; vertical-align:middle;">MoocDynasty &copy; 2014 </span>
+			<a id="sjsu" href="http://www.sjsu.edu/" target="_blank"><span><img id="sjsu-logo" width="100" src="images/sjsu.png" alt="SJSU"/></span></a> 
+			<a id="sjsucs" href="http://cs.sjsu.edu/" target="_blank"><span><img id="sjsucs-logo" width="100" src="images/sjsucs.jpg" alt="SJSU CS"/></span></a>
+		</div>
 	</div>
 </div>
