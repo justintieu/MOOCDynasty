e61a4a0692a1c02ba0543556c70adff814fbec63
Justin Tieu <tieujustin@gmail.com>
2014-06-01 00:31:52 -0700

    Updated debugging code

--- website/debuggers/fixavgrating.php
+++ website/debuggers/fixavgrating.php
@@ -1,5 +1,5 @@
 <?php 
-	require_once('connect.php');
+	require_once('../includes/connect.php');
 	$sql = "SELECT * FROM `averagerating`";
 	echo $sql."<br/>";
 	$results = $mysqli->query($sql);
--- website/debuggers/update_reviews.php
+++ website/debuggers/update_reviews.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('connect.php');
+	require_once('../includes/connect.php');
 	$sql = "SELECT * FROM `averagerating`";
 	$results = $mysqli->query($sql);
 	while($row = mysqli_fetch_array($results)) {
